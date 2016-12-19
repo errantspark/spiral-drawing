89a82d8559f9e86de1e812418166d8133b4a8f41
errantspark <errantspark@gmail.com>
2016-12-19 02:18:36 -0800

    parametrized some shit

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let body = document.body
 let drawCtx = document.getElementById('drawing').getContext('2d')
 let imgCtx = document.getElementById('image').getContext('2d')
+let prams = {
+  steps: 20000,
+  maxThick: 8.8,
+  minThick: 0.8,
+  wiggle: 1.5
+}
 let lastImg
 
 let processImg = (img, ctx, prams) => {
@@ -22,7 +28,11 @@ let processImg = (img, ctx, prams) => {
 }
 
 let drawSpiral = (drawCtx, imgCtx, prams) => {
-  let a = 1*2
+  let steps = prams.steps
+  let maxThick = prams.maxThick
+  let minThick = prams.minThick
+  let wig = prams.wiggle
+  let a = 1
   let b = 2
   let centerx = drawCtx.canvas.width / 2
   let centery = drawCtx.canvas.height / 2
@@ -30,25 +40,27 @@ let drawSpiral = (drawCtx, imgCtx, prams) => {
   drawCtx.clearRect(0, 0, drawCtx.canvas.width, drawCtx.canvas.height)
 
   drawCtx.moveTo(centerx, centery)
+  drawCtx.strokeStyle = "#000"
   let [lastx,lasty] = [centerx,centery]
-  for (i = 0; i < 19000; i++) {
-    let angle = 0.015 * i
-    let x = centerx + (a + b * angle) * Math.cos(angle) + Math.random()
-    let y = centery + (a + b * angle) * Math.sin(angle) + Math.random()
+  for (i = 0; i < steps; i++) {
+    let angle = 285/steps * i
+    let x = centerx + (a + b * angle) * Math.cos(angle) + (Math.random()*wig)
+    let y = centery + (a + b * angle) * Math.sin(angle) + (Math.random()*wig)
 
     drawCtx.beginPath()
     drawCtx.moveTo(lastx, lasty)
-    drawCtx.strokeStyle = "#000"
 
     let pxl = imgCtx.getImageData(x/2, y/2, 1, 1).data.slice(0,3)
     let pxlB = 255 - pxl.reduce((a,b) => a+b)/3
 
-    drawCtx.lineWidth = 0.6+pxlB/32
+    drawCtx.lineWidth = minThick+pxlB/(255/(maxThick-minThick))
     drawCtx.lineTo(x, y)
     drawCtx.stroke()
     ;[lastx,lasty] = [x,y]
   }
 }
+
+let redraw = () => processImg(lastImg,imgCtx,prams)
 
 let preventDefault = event => {
   event.preventDefault()
@@ -59,9 +71,9 @@ let handleDrop = event => {
   let reader = new window.FileReader()
   reader.onload = event => {
     let img = new Image
-    img.src = event.target.result;
+    img.src = event.target.result
     lastImg = img
-    img.onload = () => processImg(img,imgCtx,{})
+    img.onload = () => processImg(img,imgCtx,prams)
   }
   let file = event.dataTransfer.files[0]
   reader.readAsDataURL(file)
@@ -70,5 +82,3 @@ let handleDrop = event => {
 window.addEventListener('drop', preventDefault)
 window.addEventListener('dragover', preventDefault)
 body.addEventListener('drop', handleDrop)
-
-
