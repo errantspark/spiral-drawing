83ee72882f1a8c50c453998a1f3cdbc6c7d9af41
errantspark <errantspark@gmail.com>
2016-12-18 21:21:38 -0800

    proof of concept

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
 let body = document.body
-let ctx = document.getElementById('drawing').getContext('2d');
+let drawCtx = document.getElementById('drawing').getContext('2d');
+let imgCtx = document.getElementById('image').getContext('2d');
+let lastImg
+
+let processImg = (img, ctx, prams) => {
+  //is there a cleaner way to do this?
+  let zip = (a1,a2,fn) => a1.map((val,i) => fn(val,a2[i]))
+
+  let imgSize = [img.width,img.height]
+  let canSize = [ctx.canvas.width,ctx.canvas.height]
+
+  let sizes = zip(imgSize,canSize,Array.of)
+  let scaleF = sizes.map(e => e[0]/e[1]).reduce((a,b) => Math.min(a,b))
+  let scaledDims = imgSize.map(e => e/scaleF)
+  let imgPos = zip(scaledDims,canSize,(a,b)=>-(a-b)/2)
+  //end this 
+
+  ctx.drawImage(img,0,0,...imgSize,...imgPos,...scaledDims);
+
+  drawSpiral(drawCtx, imgCtx, prams)
+}
+
+let drawSpiral = (drawCtx, imgCtx, prams) => {
+  let a = 1
+  let b = 1.4
+  let centerx = drawCtx.canvas.width / 2;
+  let centery = drawCtx.canvas.height / 2;
+  //being spiraling :)
+  drawCtx.clearRect(0, 0, 600, 600);
+
+  drawCtx.moveTo(centerx, centery);
+  let [lastx,lasty] = [centerx,centery]
+  for (i = 0; i < 20000; i++) {
+    let angle = 0.01 * i
+    let x = centerx + (a + b * angle) * Math.cos(angle)
+    let y = centery + (a + b * angle) * Math.sin(angle)
+
+    drawCtx.beginPath();
+    drawCtx.moveTo(lastx, lasty)
+    drawCtx.strokeStyle = "#000"
+
+    let pxl = imgCtx.getImageData(x, y, 1, 1).data.slice(0,3)
+    let pxlB = 255 - pxl.reduce((a,b) => a+b)/3
+
+    drawCtx.lineWidth = 0.3+pxlB/64
+    drawCtx.lineTo(x, y)
+    drawCtx.stroke();
+    ;[lastx,lasty] = [x,y]
+  }
+}
 
 let preventDefault = event => {
   event.preventDefault()
@@ -11,20 +60,8 @@ let handleDrop = event => {
   reader.onload = event => {
     var img = new Image;
     img.src = event.target.result;
-    //is there a cleaner way to do this?
-    let imgSize = [img.width,img.height]
-    let canSize = [ctx.canvas.width,ctx.canvas.height]
-    let zip = (a1,a2,fn) => a1.map((val,i) => fn(val,a2[i]))
-    let sizes = zip(imgSize,canSize,Array.of)
-    let scaleF = sizes.map(e => e[0]/e[1]).reduce((a,b) => Math.min(a,b))
-    let scaledDims = imgSize.map(e => e/scaleF)
-    let imgPos = zip(scaledDims,canSize,(a,b)=>-(a-b)/2)
-    console.log(imgPos)
-    //end this 
-
-    img.onload = function() {
-      ctx.drawImage(img,0,0,...imgSize,...imgPos,...scaledDims);
-    }
+    lastImg = img
+    img.onload = () => processImg(img,imgCtx,{})
   }
   let file = event.dataTransfer.files[0]
   reader.readAsDataURL(file)
